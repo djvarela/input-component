9252bcc62b97e09c8ad01789f2bb71f41c08b830
Diego J. Varela <73259288+djvarela@users.noreply.github.com>
2023-08-25 16:23:06 -0300

    Update App.jsx
    News Components

--- Input/src/App.jsx
+++ Input/src/App.jsx
@@ -4,13 +4,22 @@ import { Input } from "./assets/components/Input";
 function App() {
   return (
     <>
-      <Input type="text" size="sm"/>
-      <Input type="text" size="lg"/>
-      <Input type="text" size="full"/>
-      <Input type="text" disabled={'disabled'}/>
+      <Input type="text" label='sm' size="sm"/>
+      <Input type="text" label='lg' size="lg"/>
+      <Input type="text" label='full' size="full"/>
+      <Input type="text" label='disabled' disabled={'disabled'}/>
+      <Input type="text" label='focus' color="focus"/>
+      <Input type="text"  label='error' color="error" />
+
+      <Input type="text" label="Prueba de texto" helperText='Prueba de texto'/>
+      <Input type="text"  label='Prueba de texto' helperText='Prueba de texto' color="error" />
+
+      <Input type="text" typeIcon='starIcon' icon='shop' label='error' color="error" />
+
+      <Input type="text" typeIcon='endIcon' icon='shop' label='error' color="error" />
 
 
-      <Input type="text"  color="error" />
+
     </>
   );
 }
