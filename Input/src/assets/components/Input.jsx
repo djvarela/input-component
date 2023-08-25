683bc9a361d8d87c975596e4abbc2faccca1467d
Diego J. Varela <73259288+djvarela@users.noreply.github.com>
2023-08-25 16:22:19 -0300

    Update Input.jsx
    Add atributtes

--- Input/src/assets/components/Input.jsx
+++ Input/src/assets/components/Input.jsx
@@ -1,22 +1,27 @@
 
-export const Input = ({type = 'text' , placeholder = 'Placeholder', disabled = null, size = 'sm', color = ''}) => {
+export const Input = ({typeIcon,helperText, icon ,label ='',type = 'text' , placeholder = 'Placeholder', disabled = null, size = '' , color='' }) => {
+  
  
-   
   return (
     <>
-    <span>
+    <div className="buttonContainer">
 
-    <label htmlFor="">.{`${color} ${size}`}</label>
-    
+    <label htmlFor="">{label}</label>
+    <span className={typeIcon}>
+
       <input 
        type={type}
        disabled={disabled} 
        className={`${color} ${size}`} 
        placeholder={placeholder}
-   
-      
+       
        />
+         {icon == null ? '' : <span className="material-symbols-rounded">{icon}</span> }
        </span>
+
+     {helperText == null ? '' : <small>{helperText}</small> }
+
+       </div>
     </>
   )
 }
